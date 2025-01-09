e5d8af9518533a1ed6ebe40dcd2fa6e456b86c36
BugHunter-X <askzenai@gmail.com>
2025-01-09 09:48:06 +0100

    Add tailwind.config.fixed.js file

---
+++ tailwind.config.fixed.js
@@ -0,0 +1,13 @@
+```javascript
+// tailwind.config.fixed.js
+module.exports = {
+  content: [ 
+    //Use a glob to include all files
+    './src/**/*.{html,js,jsx,ts,tsx}', 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+```
